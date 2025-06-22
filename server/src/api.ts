673b09b2db3ddf6b9314a8a59083fa5c3f2ba14f
McKay Moore <mckayreedmoore@gmail.com>
2025-06-22 12:51:58 -0600

    move server logic into correct file. add router

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,20 +1,7 @@
 import express from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
-import dotenv from 'dotenv';
-dotenv.config();
 
-const app = express();
+const router = express.Router();
 
-app.use(helmet());
-app.use(cors());
 
-function startServer(port: number = Number(process.env.PORT) || 5000) { 
-    app.listen(port, () => {
-        console.log(`Server running on port: ${port}`);
-    });
-}
 
-if (require.main === module) {
-    startServer();
-}
+export default router;
---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import router from './api'
+
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+app.use('/api' router);
+
+function startServer(port: number = Number(process.env.PORT) || 5000) { 
+    app.listen(port, () => {
+        console.log(`Server running on port: ${port}`);
+    });
+}
+
+if (require.main === module) {
+    startServer();
+}
